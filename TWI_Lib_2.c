add4636d031f015c750980ccf238cca683dc4904
Nischal Nepal <mynameisnischal2@gmail.com>
2017-05-29 19:38:07 +0545

    Update TWI_Lib_2.c

--- TWI_Lib_2.c
+++ TWI_Lib_2.c
@@ -23,7 +23,7 @@
 /************************************************************************/
 
 #ifndef F_CPU
-#define F_CPU		16000000UL
+#define F_CPU			16000000UL
 #endif
 
 #include <avr/io.h>
@@ -36,13 +36,13 @@
 #define TWI_NO_DATA		0xF8
 
 enum {
-	START			=	0x08,
-	REP_START		=	0x10,
+	START		=	0x08,
+	REP_START	=	0x10,
 	
-	SLA_W_ACK		=	0x18,
-	SLA_W_NACK		=	0x20,
-	DATA_W_ACK		=	0x28,
-	DATA_W_NACK		=	0x30,
+	SLA_W_ACK	=	0x18,
+	SLA_W_NACK	=	0x20,
+	DATA_W_ACK	=	0x28,
+	DATA_W_NACK	=	0x30,
 	
 	DATA_R_ACK,
 	DATA_R_NACK
@@ -60,12 +60,12 @@ typedef enum {
 // 	uint8_t error;
 // 	} status;
 	
-#define TWI_Send_Start			(	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWSTA)		); while(interrupt != started)
-#define TWI_Send_Stop			(	TWCR = (1 << TWEN)  | (1 << TWIE) | (1 << TWSTO)					); while(TWCR & (1 << TWSTO))
-#define TWI_Send_Rep_Start		(	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWSTA)		); while(interrupt != started)
-#define TWI_Send_Transmitt		(	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE)						)
-#define TWI_Send_ACK			(	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWEA)		)
-#define TWI_Send_NACK			(	TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE)						)
+#define TWI_Send_Start		( TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWSTA) ); while(interrupt != started)
+#define TWI_Send_Stop		( TWCR = (1 << TWEN)  | (1 << TWIE) | (1 << TWSTO)		 ); while(TWCR & (1 << TWSTO))
+#define TWI_Send_Rep_Start	( TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWSTA) ); while(interrupt != started)
+#define TWI_Send_Transmitt	( TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE)		 )
+#define TWI_Send_ACK		( TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE) | (1 << TWEA)	 )
+#define TWI_Send_NACK		( TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWIE)		 )
 
 #define TWI_TX_BUF_SIZE			32
 
